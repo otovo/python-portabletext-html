a3a96047514a70ecd6a31f46bd7a7ba357a1bc0e
Sondre Lillebø Gundersen <sondrelg@otovo.com>
2021-04-19 17:51:43 +0200

    linting and cleanup

--- sanity_html/types.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from typing import Optional
-
-    SanityIdType = Optional[str]  # represents a [:13] uuid hex
