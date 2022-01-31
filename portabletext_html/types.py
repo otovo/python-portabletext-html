b4f07fab68c3282a83960897d701d4f1f6bec10c
rootisenabled <oleksiv@otovo.com>
2022-01-31 08:56:11 +0100

    Bugfix: user-defined custom marks can be passed to Blocks again When I was trying to create my own custom mark definition, I noticed that the information from 'markDefs' property is not passed down to the renderer. `get_default_marker_definitions` method graps the context only from the default_annotations dictionary. `add_custom_marker_definitions` solves the issue by using the same logic as `get_default_marker_definitions`, but looping over user-defined annotations (self.marker_definitions).

--- portabletext_html/types.py
+++ portabletext_html/types.py
@@ -53,9 +53,7 @@ class Block:
         To make handling of span `marks` simpler, we define marker_definitions as a dict, from which
         we can directly look up both annotation marks or decorator marks.
         """
-        marker_definitions = get_default_marker_definitions(self.markDefs)
-        marker_definitions.update(self.marker_definitions)
-        self.marker_definitions = marker_definitions
+        self.marker_definitions = self._add_custom_marker_definitions()
         self.marker_frequencies = self._compute_marker_frequencies()
 
     def _compute_marker_frequencies(self) -> dict[str, int]:
@@ -67,6 +65,16 @@ class Block:
                 else:
                     counts[mark] = 0
         return counts
+
+    def _add_custom_marker_definitions(self) -> dict[str, Type[MarkerDefinition]]:
+        marker_definitions = get_default_marker_definitions(self.markDefs)
+        marker_definitions.update(self.marker_definitions)
+        for definition in self.markDefs:
+            if definition["_type"] in self.marker_definitions:
+                marker = self.marker_definitions[definition["_type"]]
+                marker_definitions[definition["_key"]] = marker
+                del marker_definitions[definition["_type"]]
+        return marker_definitions
 
     def get_node_siblings(self, node: Union[dict, Span]) -> Tuple[Optional[dict], Optional[dict]]:
         """Return the sibling nodes (prev, next) to the given node."""
